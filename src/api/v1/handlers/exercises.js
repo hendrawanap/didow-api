2400417160d30d3740d76ed657174eb50fcbfc06
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-05-19 21:24:38 +0700

    Add date range queries for getting exercises

--- src/api/v1/handlers/exercises.js
+++ src/api/v1/handlers/exercises.js
@@ -68,20 +68,56 @@ const groupExercisesByAttempts = (exercises) => {
   return transformed;
 };
 
+const getDefaultDate = (clientTimezoneOffset) => {
+  const date = new Date();
+  const HOURS_A_DAY = 24;
+  const WEEK_IN_MILLIS = 604_800_000;
+  date.setUTCHours(HOURS_A_DAY - clientTimezoneOffset, 0, 0, 0);
+  const DEFAULT_END_DATE = date.getTime() - 1;
+  const DEFAULT_START_DATE = new Date(date.getTime() - WEEK_IN_MILLIS).getTime();
+  return { DEFAULT_START_DATE, DEFAULT_END_DATE };
+};
+
+const parseDateQuery = (inputDate, clientTimezoneOffset, endOfDay = false) => {
+  const date = new Date(inputDate);
+  const HOURS_A_DAY = 24;
+
+  if (endOfDay) {
+    date.setUTCHours(HOURS_A_DAY - clientTimezoneOffset, 0, 0, 0);
+    return date.getTime() - 1;
+  }
+
+  date.setUTCHours(0 - clientTimezoneOffset, 0, 0, 0);
+  return date.getTime();
+};
+
 const getExercises = async (request, h) => {
+  const { 'x-timezone': clientTimezoneOffset } = request.headers;
+  const { DEFAULT_START_DATE, DEFAULT_END_DATE } = getDefaultDate(clientTimezoneOffset);
   const { boom } = request.server.app;
   const {
     userId,
     groupBy = 'answers',
+    startDate,
+    endDate,
   } = request.query;
 
   // Build DB queries
   const { db } = request.server.app.firestore;
   let ref = db.collection('exercises');
+
   if (userId) {
     ref = ref.where('userId', '==', userId);
   }
+
   ref = ref.orderBy('endTime', 'desc');
+
+  if (startDate && endDate) {
+    ref = ref.startAt(parseDateQuery(endDate, clientTimezoneOffset, true))
+      .endBefore(parseDateQuery(startDate, clientTimezoneOffset));
+  } else {
+    ref = ref.startAt(DEFAULT_END_DATE).endBefore(DEFAULT_START_DATE);
+  }
 
   // Execute DB queries
   let exercises;
