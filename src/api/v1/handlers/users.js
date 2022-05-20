2e08343216f1094adc2e742ad0d0fe8c392effd1
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-05-20 18:58:52 +0700

    Initialize users route

---
+++ src/api/v1/handlers/users.js
@@ -0,0 +1,22 @@
+const getUsers = async (request, h) => {
+
+};
+
+const createUser = async (request, h) => {
+
+};
+
+const getUser = async (request, h) => {
+
+};
+
+const modifyUser = async (request, h) => {
+
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+  getUser,
+  modifyUser,
+};
