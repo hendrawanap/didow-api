dc96af0cab3e01f495a79e4b7b4642e77fa91af5
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-05-21 10:56:33 +0700

    Implements getUsers

--- src/api/v1/handlers/users.js
+++ src/api/v1/handlers/users.js
@@ -1,5 +1,17 @@
 const getUsers = async (request, h) => {
+  const { db } = request.server.app.firestore;
+  const snapshot = await db.collection('users').get();
+  const users = [];
+  snapshot.forEach((doc) => {
+    const userObject = { id: doc.id, ...doc.data() };
+    users.push(userObject);
+  });
 
+  const response = {
+    data: users,
+    message: 'success',
+  };
+  return h.response(response).code(200);
 };
 
 const createUser = async (request, h) => {
