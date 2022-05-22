a6cabdec2471439e4c81f53dcfcb1afdf1e82352
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-05-22 09:53:23 +0700

    Implements assessments route validations

--- src/api/v1/validations/index.js
+++ src/api/v1/validations/index.js
@@ -1,8 +1,10 @@
 const Joi = require('joi');
+const assessments = require('./assessments')(Joi);
 const exercises = require('./exercises')(Joi);
 const words = require('./words')(Joi);
 
 module.exports = {
+  assessments,
   exercises,
   words,
 };
