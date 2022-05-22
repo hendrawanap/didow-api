888c67d9d93112f51b13838452f819255ab01023
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-05-22 09:54:00 +0700

    Implements users route validations

--- src/api/v1/validations/index.js
+++ src/api/v1/validations/index.js
@@ -1,10 +1,12 @@
 const Joi = require('joi');
 const assessments = require('./assessments')(Joi);
 const exercises = require('./exercises')(Joi);
+const users = require('./users')(Joi);
 const words = require('./words')(Joi);
 
 module.exports = {
   assessments,
   exercises,
+  users,
   words,
 };
