43c4db69793f4095e0f14b4cdf7e4b7922f242d1
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-05-26 15:36:50 +0700

    Implements suggestions routes

--- src/api/v1/handlers/suggestions.js
+++ src/api/v1/handlers/suggestions.js
@@ -1,4 +1,35 @@
 const getSuggestion = async (request, h) => {
+  const { userId } = request.query;
+  const { db } = request.server.app.firestore;
+  const doc = await db.collection('users').doc(userId).get();
+
+  if (!doc.exists) {
+    const { boom } = request.server.app;
+    return boom.notFound();
+  }
+
+  const { weightPoint } = doc.data();
+
+  const SUGGESTIONS = {
+    BEGINNER: 'Ini rekomendasi untuk kelas pemula',
+    INTERMEDIATE: 'Ini rekomendasi untuk kelas menengah',
+    EXPERT: 'Ini rekomendasi untuk kelas ahli',
+  };
+
+  const response = {
+    data: '',
+    message: 'success',
+  };
+
+  if (weightPoint < 100) {
+    response.data = SUGGESTIONS.BEGINNER;
+  } else if (weightPoint < 300) {
+    response.data = SUGGESTIONS.INTERMEDIATE;
+  } else {
+    response.data = SUGGESTIONS.EXPERT;
+  }
+
+  return h.response(response).code(200);
 };
 
 module.exports = { getSuggestion };
