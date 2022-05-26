12dce71b1156f2b046aa4409b26fad8657e6923a
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-05-26 14:42:37 +0700

    Implements getAvatars handler

--- src/api/v1/handlers/avatars.js
+++ src/api/v1/handlers/avatars.js
@@ -1,5 +1,43 @@
 const getAvatars = async (request, h) => {
+  const { userId: uidQuery } = request.query;
+  const { db } = request.server.app.firestore;
 
+  let ref = db.collection('avatars');
+
+  if (uidQuery) {
+    ref = ref.where('userId', '==', uidQuery);
+  }
+
+  const snapshot = await ref.get();
+  const avatars = [];
+  snapshot.forEach((doc) => {
+    const {
+      topItem,
+      bodyItem,
+      bottomItem,
+      ownedItems,
+      rewardPoint,
+      userId,
+    } = doc.data();
+    const avatarObject = {
+      id: doc.id,
+      userId,
+      equippedItems: {
+        top: topItem,
+        body: bodyItem,
+        bottom: bottomItem,
+      },
+      rewardPoint,
+      ownedItems,
+    };
+    avatars.push(avatarObject);
+  });
+
+  const response = {
+    data: avatars,
+    message: 'success',
+  };
+  return h.response(response).code(200);
 };
 
 const createAvatar = async (request, h) => {
