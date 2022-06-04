e872c3889953472b41be230ff1f4c705b78a4488
HENDRAWAN ANANTHA <onyxtata11@gmail.com>
2022-06-05 00:24:46 +0700

    Implemented firebase token verification as auth mechanism

--- src/api/v1/server.js
+++ src/api/v1/server.js
@@ -3,6 +3,7 @@ const hapi = require('@hapi/hapi');
 const boom = require('@hapi/boom');
 const routes = require('./routes/index');
 const firebase = require('./services/firebase');
+const auth = require('./auth');
 
 const init = async () => {
   const server = hapi.server({
@@ -15,10 +16,12 @@ const init = async () => {
     },
   });
 
-  const { firestore } = await firebase.init();
+  const { firestore, auth: firebaseAuth } = await firebase.init();
+  server.app.auth = firebaseAuth;
   server.app.firestore = firestore;
   server.app.boom = boom;
 
+  await server.register(auth);
   await server.register(routes);
   await server.start();
 
--- src/api/v1/services/firebase.js
+++ src/api/v1/services/firebase.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+const { getAuth } = require('firebase-admin/auth');
 const serviceAccount = require('../config/service-account');
 
 const init = async () => {
@@ -8,8 +9,15 @@ const init = async () => {
     credential: cert(serviceAccount),
   });
   const db = getFirestore();
+  const auth = getAuth();
   return {
     firestore: { db, Timestamp, FieldValue },
+    auth: {
+      verifyToken: async (token) => {
+        const { uid } = await auth.verifyIdToken(token);
+        return uid;
+      },
+    },
   };
 };
 
